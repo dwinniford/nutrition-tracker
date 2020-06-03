8c926ddf4f0c7c9550ea7f32f5e29eed41488902
Dave Winniford <dwinniford@gmail.com>
2020-06-03 12:35:53 -0400

    menuplans display show container

---
+++ src/containers/MenuPlanShowContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MenuPlanShowContainer extends Component {
+    render() {
+        return (
+            <div>
+                {/* <h1>{this.props.menuPlan.title}</h1> */}
+                <h1>Test show page</h1>
+            </div>
+        )
+    }
+}
--- src/containers/MenuPlansContainer.js
+++ src/containers/MenuPlansContainer.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+import MenuPlanShowContainer from './MenuPlanShowContainer.js'
 
 export default class MenuPlansContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuPlans: [
+                {title: "Mediteranean Day"},
+                {title: "Middleaster Week"},
+                {title: "Mexican meal plan"}
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Menu Plans List</h1>
+                <ul>
+        {this.state.menuPlans.map((plan, idx) => <li><Link to={`/menuplans/${idx}`}>{plan.title}</Link></li>)}
+                </ul>
+                <Route exact path={`${this.props.match.url}/:menuPlanId`} component={MenuPlanShowContainer} />
             </div>
         )
     }
