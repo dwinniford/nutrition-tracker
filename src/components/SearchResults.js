4f43b07351eeee8ff3d38f9942dfcb4cbc82dd9f
Dave Winniford <dwinniford@gmail.com>
2020-06-04 09:43:05 -0400

    adds search results component

---
+++ src/components/SearchResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Results:</h3>
+                <p>fake results</p>
+            </div>
+        )
+    }
+}
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import SearchForm from '../components/SearchForm.js'
+import {connect} from 'react-redux'
+import SearchResults from '../components/SearchResults.js'
 
-export default class SearchContainer extends Component {
+class SearchContainer extends Component {
     render() {
         return (
             <div>
                 <SearchForm />
+                <SearchResults />
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        results: state.searchResults
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        search: (text) => dispatch({type: 'SEARCH', text})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchContainer)
