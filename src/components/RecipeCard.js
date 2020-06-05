37ab0bc68ded764138c8768b21446ae52feffaf9
Dave Winniford <dwinniford@gmail.com>
2020-06-05 10:01:50 -0400

    toggles recipe nutrition card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
-
+import RecipeNutritionCard from './RecipeNutritionCard.js'
 
 export default class RecipeCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            displayNutrition: false
+        }
+    }
+
+    displayContent = () => {
+        if (this.state.displayNutrition) {
+            return <RecipeNutritionCard />
+        } else {
+            return <img src={this.props.recipe.image} />
+        }
+    }
     
     handleAddRecipe = () => {
         this.props.addRecipe(this.props.recipe)
+    }
+
+    handleContentToggle = () => {
+        this.state.displayNutrition ? this.setState({displayNutrition: false}) : this.setState({displayNutrition: true})
     }
     
     render() {
         return (
             <div className="recipe-card">
-                <img src={this.props.recipe.image} />
+                {this.displayContent()}
                 <div className="card-buttons">
                     <a className="black-button" href={this.props.recipe.url}>{this.props.recipe.label}</a>
                     <button className="black-button" onClick={this.handleAddRecipe}>
                         Add to Meal Plan
                     </button>
-                    <button className="black-button">Nutrition Info</button>
+                    <button className="black-button" onClick={this.handleContentToggle}>
+                        Nutrition Info
+                    </button>
                     <p>from: {this.props.recipe.source}</p>
                 </div>
             </div>
---
+++ src/components/RecipeNutritionCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RecipeNutritionCard extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Nutrition</h3>
+            </div>
+        )
+    }
+}
