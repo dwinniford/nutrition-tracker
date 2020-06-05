efd8ce684c2cadfb6241cd6e90d64223d89ce2d5
Dave Winniford <dwinniford@gmail.com>
2020-06-05 13:11:40 -0400

    drag event sets drop zone

--- src/containers/MealPlansNewContainer.js
+++ src/containers/MealPlansNewContainer.js
@@ -4,6 +4,12 @@ import RecipeCard from '../components/RecipeCard.js'
 import './MealPlansNewContainer.css'
 
 class MealPlansNewContainer extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            dropZoneId: null
+        }
+    }
     
     recipeButton = (recipe) => {
     return <button draggable className="black-button">{recipe.label}</button>
@@ -11,22 +17,24 @@ class MealPlansNewContainer extends Component {
 
     handleDrop = (event) => {
         event.preventDefault()
-        console.log("Dropping")
+        console.log("Dropping", event.target.innerText)
     }
 
     handleDragOver = (event) => {
         event.preventDefault()
-        console.log("Dragging over")
+        console.log("Dragging over", event.target.dataset.dayId)
+        this.setState({dropZoneId: event.target.dataset.dayId})
     }
 
     render() {
         return (
             <div>
                 <h1>New Meal Plan</h1>
+        <p>drop zone: {this.state.dropZoneId}</p>
                 <div className="newRecipes">
                     {this.props.newMealPlan.map(recipe => this.recipeButton(recipe))}
                 </div>
-                <div onDragOver={this.handleDragOver} onDrop={this.handleDrop} className="day-1" >
+                <div data-day-id="1" onDragOver={this.handleDragOver} onDrop={this.handleDrop} className="day-1" >
                     <h3 >Day 1</h3>
 
                 </div>
