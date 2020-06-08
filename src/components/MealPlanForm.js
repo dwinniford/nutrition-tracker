66d239b9ce4f0238fc58f53b908d0a0c641c0895
Dave Winniford <dwinniford@gmail.com>
2020-06-08 07:59:22 -0400

    title form is controlled

--- src/components/MealPlanForm.js
+++ src/components/MealPlanForm.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class MealPlanForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({text: event.target.value})
+    }
+    
     render() {
         return (
             <div>
                 <form>
                     <label>Title</label>
-                    <input type="text"/>
+                    <input onChange={this.handleChange} type="text" value={this.state.text}/>
                     <input className="black-button" type="submit"/>
                 </form>
             </div>
