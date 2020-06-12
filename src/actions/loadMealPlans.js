6bd1859d308c19848923222315641514c85a9661
Dave Winniford <dwinniford@gmail.com>
2020-06-12 15:56:19 -0400

    clean up search

--- src/actions/loadMealPlans.js
+++ src/actions/loadMealPlans.js
@@ -7,20 +7,10 @@ function getCSRFToken() {
 export const loadMealPlans = () => {
     return (dispatch) => {
         dispatch({type: "LOADING_MEALPLANS"})
-        // let configObj = {
-        //     method: 'POST',
-        //     credentials: 'include',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'X-CSRF-Token': getCSRFToken()
-        //     },
-        //     body: JSON.stringify(mealPlan)
-        // }
         fetch(BACKEND_URL + "/meal_plans")
             .then(resp => resp.json())
             .then(function(json) {
                 dispatch({type: "LOADED_MEALPLANS", mealPlans: json})
-    
             })
     }
 }
