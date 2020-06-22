4fac58b49a4d33d7d625fef9e5ccc67e6238f675
Dave Winniford <dwinniford@gmail.com>
2020-06-22 16:29:38 -0400

    refactor day card as functional component

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import DayNutritionCard from './DayNutritionCard.js'
 
-export default class DayCard extends Component {
+export default function DayCard(props) {
     
-    displayDropZone = () => {
-        switch(this.props.drag) {
+    const displayDropZone = () => {
+        switch(props.drag) {
             case "dragging":
                 return "drop-zone"
             case null:
@@ -15,43 +15,43 @@ export default class DayCard extends Component {
         
     }
 
-    renderRecipeButtons = () => {
-        return this.props.recipes.map(recipe => {
+    const renderRecipeButtons = () => {
+        return props.recipes.map(recipe => {
             return (
                 <button 
                     className="black-button-container"
                 >
                     {recipe.label}
                     <span className="corner-x"
-                    data-day-id={this.props.day}
+                    data-day-id={props.day}
                     data-name={recipe.label} 
-                    onClick={this.props.handleUnassignDay}>x</span>
+                    onClick={props.handleUnassignDay}>x</span>
                 </button>
             )
         })    
     }
 
-    render() {
-        return (
-            
-                <div className="day-card" >
-                    <button data-day-id={this.props.day} 
-                    onClick={this.props.handleRemoveDay} 
-                    className='corner-x'>x</button>
-                    <h3
-                     className={this.displayDropZone()}
-                    data-day-id={this.props.day} 
-                    onDragOver={this.props.handleDragOver}
-                    onDrop={this.props.handleDrop}
-                    >Day {this.props.day}
-                    </h3>
-                    <div className="recipe-buttons-container">
-                        {this.renderRecipeButtons()}
-                    </div>
-                    <h3>Nutrition Summary</h3>
-                    <DayNutritionCard recipes={this.props.recipes} />
-                </div>
-            
-        )
-    }
+    
+    return (
+        
+        <div className="day-card" >
+            <button data-day-id={props.day} 
+            onClick={props.handleRemoveDay} 
+            className='corner-x'>x</button>
+            <h3
+                className={displayDropZone()}
+            data-day-id={props.day} 
+            onDragOver={props.handleDragOver}
+            onDrop={props.handleDrop}
+            >Day {props.day}
+            </h3>
+            <div className="recipe-buttons-container">
+                {renderRecipeButtons()}
+            </div>
+            <h3>Nutrition Summary</h3>
+            <DayNutritionCard recipes={props.recipes} />
+        </div>
+        
+    )
+    
 }
